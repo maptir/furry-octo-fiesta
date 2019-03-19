78585ad99c09d1506e8e41e15fb9f4cb86f15a10
Archawin <zepalzth@gmail.com>
2019-03-20 00:01:37 +0700

    Tum tum mai wa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { reverse, sortBy, prop } from 'ramda'
+import messages from './data/messages'
+import profile from './data/profile'
 import './App.css';
 
 class App extends Component {
+  state = {
+    sortByLongestConversation: sortBy(prop('conversation')),
+    message: messages[0]
+  }
+
+  heartCount = () => {
+    return this.state.message.conversation.reduce((accum, current) => 
+      current.heart ? accum + 1 : accum, 0)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{this.state.message.participants[1] === profile.username ? 
+          this.state.message.participants[0] : this.state.message.participants[1]}
+          : {this.state.message.conversation.length} Chats</h1>
+        <div className="row">
+          <div className="chat">
+            <div>Heart Count: {this.heartCount()}</div>
+          </div>
+          <div className="chat">
+            {
+              reverse(this.state.message.conversation).map(chat => {
+                const date = new Date(chat.created_at)
+                return (
+                <div 
+                  style={{ 
+                    textAlign: chat.sender === profile.username ? 'right' : 'left', 
+                    margin: '0.5em',
+                    borderRadius: '24px',
+                  }}
+                  onClick={() => {console.log(chat)}}
+                >
+                  <div style={{fontSize: '10px'}}>{chat.story_share}</div>
+                  {chat.text}
+                  {chat.heart}
+                  <div className="date">{`${date.getHours()}:${date.getMinutes()} ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`}</div>
+                </div>
+              )})
+            }
+          </div>
+          <div className="chat">
+            {
+              reverse(this.state.sortByLongestConversation(messages)).map(message => (
+                <div style={{ cursor: 'pointer' }} onClick={() => this.setState({message})}>
+                  {message.participants[1] === profile.username ? message.participants[0] : message.participants[1]}
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </div>
     );
   }
